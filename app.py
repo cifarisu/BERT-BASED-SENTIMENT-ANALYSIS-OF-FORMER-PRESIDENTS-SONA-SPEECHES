3f485599638f3a831ad0b8a6a5d74bd9696609d4
cifcs2 <129279134+cifcs2@users.noreply.github.com>
2023-05-13 03:54:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
+# Set title and favicon
+st.set_page_config(page_title="SONA Sentiment", page_icon="icon.png")
+
 # Set default page to "Predict
 page = "Predict"
 
