23412a8c6902b36a49ce7d0f1003c6d4d24e4706
cifcs2 <129279134+cifcs2@users.noreply.github.com>
2023-05-13 03:36:19 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
-# Set default page to "Explore"
-page = "Explore"
+# Set default page to "Predict
+page = "Predict"
 
 # Display sidebar to select page
-page = st.sidebar.selectbox("Explore or Predict", ("Explore", "Predict"))
+page = st.sidebar.selectbox("Predict or Explore", ("Predict", "Explore"))
 
 # Show appropriate page based on user selection
-if page == "Explore":
-    show_explore_page()
-else:
+if page == "Predict":
     show_predict_page()
+else:
+    show_explore_page()
