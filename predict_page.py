ea2324a725aaaa04a64fb395ff19db6b18d8e33d
cifcs2 <129279134+cifcs2@users.noreply.github.com>
2023-05-15 03:48:39 +0800

    Add files via upload

--- predict_page.py
+++ predict_page.py
@@ -23,7 +23,7 @@ model.load_state_dict(torch.load('best_model.pth', map_location=torch.device('cp
 model.eval()
 tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 
-target_names = ['POSITIVE', 'NEGATIVE']
+target_names = ['NEGATIVE', 'POSITIVE']
 MAX_LEN = 160
 
 # Assuming you have already defined `tokenizer`, `model`, `device`, and `target_names`
