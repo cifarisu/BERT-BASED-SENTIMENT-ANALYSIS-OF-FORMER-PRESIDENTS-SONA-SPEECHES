b8883761ffc4fb5738de9f49a6f9203b06d51086
cifcs2 <129279134+cifcs2@users.noreply.github.com>
2023-05-17 00:39:49 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import streamlit as st
 from predict_page import show_predict_page
+from sentence_prediction import show_sentence_prediction
 from explore_page import show_explore_page
 
 # Set title and favicon
 st.set_page_config(page_title="SONA Sentiment", page_icon="icon.png")
 
-# Set default page to "Predict
-page = "Predict"
+# Set default page to "Speech Prediction"
+page = "Speech Prediction"
 
 # Display sidebar to select page
-page = st.sidebar.selectbox("Predict or Explore", ("Predict", "Explore"))
+page = st.sidebar.selectbox("Select an option", ("Speech Prediction", "Single Prediction", "Presidents"))
 
 # Show appropriate page based on user selection
-if page == "Predict":
+if page == "Speech Prediction":
     show_predict_page()
+elif page == "Single Prediction":
+    show_sentence_prediction()
 else:
     show_explore_page()
